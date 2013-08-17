5693fc7c7087df677b1f4dffbdf5a18a84db856b
Juha Paananen <juha@juha-paananens-macbook.local>
2013-08-17 21:15:53 +0300

    add w for waiting

--- Napero.hs
+++ Napero.hs
@@ -1,9 +1,12 @@
 module Napero where
 
 import System.Process
+import Control.Concurrent
 
 say x = do
   readProcessWithExitCode "say" [x] []
   return ()
 p = putStrLn
 i = getLine
+w delay = threadDelay $ delay * 1000000
+
